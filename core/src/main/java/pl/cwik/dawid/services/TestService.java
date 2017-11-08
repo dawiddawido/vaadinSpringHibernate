f6dd081c119bc6357637d48c2e4fc040dade093b
Dawid Ćwik <dawid.cwik@silvermedia.pl>
2017-11-08 09:24:41 +0100

    add User class to test service

--- core/src/main/java/pl/cwik/dawid/services/TestService.java
+++ core/src/main/java/pl/cwik/dawid/services/TestService.java
@@ -1,10 +1,13 @@
 package pl.cwik.dawid.services;
 
+import model.User;
+
 /**
  * Created by Dawid on 05.10.2017.
  */
 public class TestService {
     public TestService(){
         System.out.println("Core start!");
+        User user = new User();
     }
 }
